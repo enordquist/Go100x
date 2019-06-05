6634b2f0f7698fb84c60336784bce05a3db7c403
Erik Nordquist <enordquist@login1.ascent.olcf.ornl.gov>
2019-06-05 13:17:59 -0400

    CPU matrix

--- source/Go100x/src/kernels.cpp
+++ source/Go100x/src/kernels.cpp
@@ -4,4 +4,27 @@
 //======================================================================================//
 // calculate on the CPU
 //
-void cpu_calculate(int block, int ngrid, float* matrix_a, float* matrix_b, int size) {}
+void cpu_calculate(int block, int ngrid, float* matrix_a, float* matrix_b, int size) 
+{
+  for(int i = 0; i < size; i++)
+  {
+    std::cout << matrix_a[i] << matrix_b[i] << std::endl;
+
+  };
+}
+
+int main()
+{
+  int size = 100;
+
+  float a[size];
+  float b[size];
+  for (int i = 0; i < size; i++ ) 
+  {
+    a[i] = i;
+    b[i] = i;
+  };
+  cpu_calculate(1, 1, a, b, 5);
+
+  return 0;
+}
