7cd13a2daf79756486e47d9beb1efb2b8405d887
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2019-06-07 08:35:51 -0700

    Updated examples/mult.py

--- examples/mult.py
+++ examples/mult.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
-
+import os
 import go100x
 import numpy as np
 
-n = 100000000
+os.environ["TIMEMORY_PRECISION"] = "8"
+
+n = int(2.0e8)
 a = np.zeros([n], dtype=np.float)
 b = np.zeros([n], dtype=np.float)
 a += 2
@@ -12,13 +14,13 @@ b += 3
 
 c = go100x.calculate_cpu(a, b)
 
+go100x.set_device(0)
+
 o = [32, 64, 128, 256, 512, 1024]
 for block in o:
     for ngrid in o + [-1]:
         if ngrid < 0:
             ngrid = int((n + block - 1) / block)
-        d = go100x.calculate_gpu(block, ngrid, a, b)
+        d = go100x.calculate_gpu([ngrid], [block], a, b)
 
 print("\nResults for array of size {} ({})", n, float(n))
-#print(c)
-#print(d)
