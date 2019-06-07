d23dbdc611e6e6cbfbb94ace2159f48e25e5672b
Erik Nordquist <erikbnordquist@gmail.com>
2019-06-07 09:35:19 -0400

    changes to fun.py

--- examples/fun.py
+++ examples/fun.py
@@ -1,8 +1,8 @@
 import go100x
 import numpy as np
 
-sizea=855*3 #000000
-sizeb=798*3
+sizea=855 #*3
+sizeb=798 #*3
 #sizea=9
 #sizeb=10
 
@@ -14,12 +14,18 @@ b = np.zeros(sizeb)
 a=a+2
 b=b+3
 
-#a = np.random.rand(sizea)
-#b = np.random.rand(sizeb)
+a = np.random.rand(sizea)
+b = np.random.rand(sizeb)
 
 # cpu version of function
 c = go100x.fun_cpu(a,b)
 
+##  gpu_funv0: deprecated algorithm
+g0 = go100x.fun_gpu([855,1,1],[256,1,1],a,b)
+
+print("max error:",max(abs(c-g0)))
+#quit()
+## gpu_funv1: better algorithm
 # number of threads per block
 for blockx in [128,256,512,1024]:
 
@@ -43,9 +49,11 @@ for blockx in [128,256,512,1024]:
                ngrid = [gridx,gridy,gridz]
                
                # gpu version of function
-               d = go100x.funv1_gpu(ngrid,block,a,b)
+               g1 = go100x.funv1_gpu(ngrid,block,a,b)
                
                # error check
                print(c[1:10])
-               print(d[1:10])
-               print("ngrid:",ngrid,"block:", block,"max error:",max(abs(c-d)))
+               print(g0[1:10])
+               print(g1[1:10])
+               print("                               v0 max error:",max(abs(c-g0)))
+               print("ngrid:",ngrid,"block:", block,"v1 max error:",max(abs(c-g1)))
