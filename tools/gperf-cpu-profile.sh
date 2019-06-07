327a8f767a68d7b42e6cd0aea11b2d9cc2d8b5ea
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2019-06-07 12:18:09 -0400

    Updated gperf

--- tools/gperf-cpu-profile.sh
+++ tools/gperf-cpu-profile.sh
@@ -1,87 +1,97 @@
-#!/bin/bash
+#!/bin/bash -e
 
-set -o errexit
+EXE=$(basename ${1})
+DIR=cpu.prof.${EXE}
+mkdir -p ${DIR}
 
 : ${N:=0}
-: ${CPU_PROFILE:=""}
-: ${CPUPROFBASE:=gperf.cpu.prof.$(basename ${1})}
-: ${PPROF_ARGS:=""}
+: ${GPERF_PROFILE:=""}
+: ${GPERF_PROFILE_BASE:=${DIR}/gperf}
+: ${PPROF_ARGS:="--no_strip_temp --lines"}
 : ${MALLOCSTATS:=1}
 : ${CPUPROFILE_FREQUENCY:=500}
 : ${INTERACTIVE:=0}
 
-while [ -z "${CPU_PROFILE}" ]
+while [ -z "${GPERF_PROFILE}" ]
 do
-    TEST_FILE=${CPUPROFBASE}.${N}
+    TEST_FILE=${GPERF_PROFILE_BASE}.${N}
     if [ ! -f "${TEST_FILE}" ]; then
-        CPU_PROFILE=${TEST_FILE}
+        GPERF_PROFILE=${TEST_FILE}
     fi
     N=$((${N}+1))
 done
 
-echo -e "\n\t--> Outputting profile to '${CPU_PROFILE}'...\n"
+export MALLOCSTATS
+export CPUPROFILE_FREQUENCY
+echo -e "\n\t--> Outputting profile to '${GPERF_PROFILE}'...\n"
 
 # remove profile file if unsucessful execution
-cleanup-failure() { set +v ; rm -f ${CPU_PROFILE}; }
+cleanup-failure() { set +v ; echo "failure"; rm -f ${GPERF_PROFILE}; }
 trap cleanup-failure SIGHUP SIGINT SIGQUIT SIGILL SIGABRT SIGKILL
 
-# configure pre-loading of profiler library
-LIBS=$(find ${PWD} -type f | egrep '\.so|\.dylib' | egrep -v 'vdt|\.o$|\.cmake$|\.txt$|\.sh$|\.a$|\.dSYM')
-LIBS=$(echo ${LIBS} | sed 's/ /:/g')
-if [ "$(uname)" = "Darwin" ]; then
-    for i in $(otool -L ${1} | egrep 'tcmalloc|profiler' | awk '{print $1}')
+ADD_LIBS()
+{
+    for i in $@
     do
-        if [ ! -d ${i} ]; then
-            LIBS=${LIBS}:${i}
+        if [ -z "${ADD_LIB_LIST}" ]; then
+            ADD_LIB_LIST="--add_lib=${i}"
+        else
+            ADD_LIB_LIST="${ADD_LIB_LIST} --add_lib=${i}"
         fi
     done
+}
+
+ADD_PRELOAD()
+{
+    for i in $@
+    do
+        if [ -z "${LIBS}" ]; then
+            LIBS=${i}
+        else
+            LIBS="${LIBS}:${i}"
+        fi
+    done
+}
+
+# configure pre-loading of profiler library
+PROJECT_LIBRARIES="$(find $PWD | egrep 'libOpenMM|libmcbaro' | egrep -v '\.a$|\.dSYM')"
+ADD_LIBS ${PROJECT_LIBRARIES}
+ADD_PRELOAD ${PROJECT_LIBRARIES}
+if [ "$(uname)" = "Darwin" ]; then
+    ADD_PRELOAD $(otool -L ${1} | egrep 'profiler' | awk '{print $1}')
     LIBS=$(echo ${LIBS} | sed 's/^://g')
     if [ -n "${LIBS}" ]; then
         export DYLD_FORCE_FLAT_NAMESPACE=1
         export DYLD_INSERT_LIBRARIES=${LIBS}
         echo "DYLD_INSERT_LIBRARIES=${DYLD_INSERT_LIBRARIES}"
     fi
-    unset LIBS
 else
-    for i in $(ldd ${1} | egrep 'tcmalloc|profiler' | awk '{print $(NF-1)}')
-    do
-        if [ ! -d ${i} ]; then
-            LIBS=${LIBS}:${i}
-        fi
-    done
+    ADD_PRELOAD $(ldd ${1} | egrep 'profiler' | awk '{print $(NF-1)}')
     LIBS=$(echo ${LIBS} | sed 's/^://g')
-    if [ -n "${LIBS}" ]; then
+    if [ -n "${LIB}" ]; then
         export LD_PRELOAD=${LIBS}
         echo "LD_PRELOAD=${LD_PRELOAD}"
     fi
-    unset LIBS
 fi
 
-export MALLOCSTATS
-export CPUPROFILE_FREQUENCY
 # run the application
-eval CPUPROFILE=${CPU_PROFILE} $@ | tee ${CPU_PROFILE}.log
-
-# failures below should not cause "test" to fail
-set +e
-set -v
+eval CPUPROFILE_FREQUENCY=${CPUPROFILE_FREQUENCY} CPUPROFILE=${GPERF_PROFILE} $@ | tee ${GPERF_PROFILE}.log
 
 # generate the results
 EXT=so
 if [ "$(uname)" = "Darwin" ]; then EXT=dylib; fi
-if [ -f "${CPU_PROFILE}" ]; then
+if [ -f "${GPERF_PROFILE}" ]; then
     : ${PPROF:=$(which google-pprof)}
     : ${PPROF:=$(which pprof)}
-    ADD_LIBS=""
-    for i in *.${EXT}
-    do
-        ADD_LIBS="${ADD_LIBS} --add_lib=${i}"
-    done
     if [ -n "${PPROF}" ]; then
-        eval ${PPROF} --text ${ADD_LIBS} ${PPROF_ARGS} ${1} ${CPU_PROFILE} | egrep -v ' 0x[0-9]' &> ${CPU_PROFILE}.txt
-        eval ${PPROF} --text --cum ${ADD_LIBS} ${PPROF_ARGS} ${1} ${CPU_PROFILE} | egrep -v ' 0x[0-9]' &> ${CPU_PROFILE}.cum.txt
+        eval ${PPROF} --text ${ADD_LIB_LIST} ${PPROF_ARGS} ${1} ${GPERF_PROFILE} | egrep -v ' 0x[0-9]' &> ${GPERF_PROFILE}.txt
+        eval ${PPROF} --text --cum ${ADD_LIB_LIST} ${PPROF_ARGS} ${1} ${GPERF_PROFILE} | egrep -v ' 0x[0-9]' &> ${GPERF_PROFILE}.cum.txt
         if [ "${INTERACTIVE}" -gt 0 ]; then
-            eval ${PPROF} ${ADD_LIBS} ${PPROF_ARGS} ${1} ${CPU_PROFILE}
+            eval ${PPROF} ${ADD_LIB_LIST} ${PPROF_ARGS} ${1} ${GPERF_PROFILE}
         fi
+    else
+        echo -e "google-pprof/pprof not found!"
     fi
+else
+    echo -e "profile file \"${GPERF_PROFILE}\" not found!"
 fi
